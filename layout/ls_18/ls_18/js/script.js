92f3f88ebce900ec96dff2d1ea36967af01eb313
PashKo <rabota.pashok@gmail.com>
2023-12-13 21:58:01 +0200

    ls_18-q

--- layout/ls_18/ls_18/js/script.js
+++ layout/ls_18/ls_18/js/script.js
@@ -1,56 +1,20 @@
 "use strict"
-
-function getLengthEl(svgElement) {
-	if (!svgElement) {
-		console.error("SVG element not provided.")
-		return
-	}
-
-	const circleElement = svgElement.querySelector("circle")
-	if (!circleElement) {
-		console.error("Circle element not found.")
-		return
-	}
-
-	const totalLength = circleElement.getTotalLength()
-	return { totalLength, circleElement }
-}
-
-function changeCounter(numberElement) {
-	if (numberElement) {
-		const valueAttribute = numberElement.getAttribute("value")
-		const value = valueAttribute ? parseInt(valueAttribute) : 0
-		const duration = numberElement.getAttribute("duration")
-		const counterInterval = (duration * 1000) / value
-		let counter = 0
-
-		const intervalId = setInterval(() => {
-			if (counter === value) clearInterval(intervalId)
-			else {
-				counter += 1
-				numberElement.innerHTML = counter + "%"
-			}
-		}, counterInterval)
-
-		return { value, duration }
-	}
-}
-function setAnimationProperties(numberElement, svgElement) {
-	const { totalLength, circleElement } = getLengthEl(svgElement)
-	const { value, duration } = changeCounter(numberElement)
-
-	const offsetValue = totalLength - (totalLength * value) / 100
-	circleElement.style.setProperty("--custom-stroke-dashoffset", offsetValue)
-
-	circleElement.style.animationDuration = duration + "s"
+function toggleClassOnElement(elClassName, addClassName) {
+	const element = document.querySelector(elClassName)
+	element?.classList.toggle(addClassName) || console.assert(false, "element not found")
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	const numberElements = document.querySelectorAll(".number")
-	const svgElements = document.querySelectorAll(".circle")
+	const iconMenu = document.querySelector(".icon-menu")
+	const menuItem = document.querySelector(".menu__item--parent")
 
-	numberElements.forEach((numberElement, index) => {
-		const svgElement = svgElements[index]
-		setAnimationProperties(numberElement, svgElement)
+
+	iconMenu.addEventListener("click", function () {
+		toggleClassOnElement(".icon-menu", "menu-open")
+		toggleClassOnElement(".menu__body", "menu-open")
+	})
+
+	menuItem.addEventListener('click', function(){
+		toggleClassOnElement(".sub-menu", ".sub-menu-open")
 	})
 })
