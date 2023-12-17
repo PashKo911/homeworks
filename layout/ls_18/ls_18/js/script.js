0618766bddb4c3de664dae314110778915f81d7a
PashKo <rabota.pashok@gmail.com>
2023-12-17 12:06:24 +0200

    ls_18_completed

--- layout/ls_18/ls_18/js/script.js
+++ layout/ls_18/ls_18/js/script.js
@@ -1,14 +1,21 @@
 "use strict"
+
 function toggleClassOnElement(elClassName, addClassName) {
 	const element = document.querySelector(elClassName)
-	element?.classList.toggle(addClassName) || console.assert(false, "element not found")
+	if (element) {
+		element.classList.toggle(addClassName)
+	} else {
+		console.assert(false, "element not found")
+	}
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 	const iconMenu = document.querySelector(".icon-menu")
 
-	iconMenu.addEventListener("click", function () {
-		toggleClassOnElement(".icon-menu", "menu-open")
-		toggleClassOnElement(".menu__body", "menu-open")
-	})
+	if (iconMenu) {
+		iconMenu.addEventListener("click", function () {
+			toggleClassOnElement(".icon-menu", "menu-open")
+			toggleClassOnElement(".menu__body", "menu-open")
+		})
+	}
 })
