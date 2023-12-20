8f7d3d37c8941fd5b7ae472d261fe87f7c96e142
PashKo <rabota.pashok@gmail.com>
2023-12-20 21:13:59 +0200

    ls_19_valid

--- layout/ls_19/ls_19/js/script.js
+++ layout/ls_19/ls_19/js/script.js
@@ -1,21 +1,10 @@
 "use strict"
 
-function toggleClassOnElement(elClassName, addClassName) {
-	const element = document.querySelector(elClassName)
-	if (element) {
-		element.classList.toggle(addClassName)
-	} else {
-		console.assert(false, "element not found")
-	}
+window.onload = ()=>{
+	const icon = document.querySelector('.icon-menu');
+	icon.addEventListener('click', function () {
+		document.documentElement.classList.toggle('menu-open');
+	});
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-	const iconMenu = document.querySelector(".icon-menu")
 
-	if (iconMenu) {
-		iconMenu.addEventListener("click", function () {
-			toggleClassOnElement(".icon-menu", "menu-open")
-			toggleClassOnElement(".menu__body", "menu-open")
-		})
-	}
-})
