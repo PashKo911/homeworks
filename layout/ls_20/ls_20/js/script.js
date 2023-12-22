923bd3c02e6f1c8451d975109c8ab4d04ea91955
PashKo <rabota.pashok@gmail.com>
2023-12-22 22:02:02 +0200

    ls_20_q

---
+++ layout/ls_20/ls_20/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+window.onload = ()=>{
+	const icon = document.querySelector('.icon-menu');
+	icon.addEventListener('click', function () {
+		document.documentElement.classList.toggle('menu-open');
+	});
+}
+
+
