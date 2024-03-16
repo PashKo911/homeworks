518fd4401bd3bdd478a5c63ed00fdc8142d96b00
PashKo <rabota.pashok@gmail.com>
2024-03-16 11:51:08 +0200

    path

--- challenge/realco/js/app.js
+++ challenge/realco/js/app.js
@@ -1055,7 +1055,7 @@
                 el.addEventListener("click", (function(e) {
                     const target = e.target.closest(".image-zoom"), rect = target.getBoundingClientRect();
                     target.classList.toggle("-active");
-                    target.style.setProperty("--image", `url(${target.querySelector("img").getAttribute("src")})`);
+                    target.style.setProperty("--image", `url(../${target.querySelector("img").getAttribute("src")})`);
                     target.style.setProperty("--x", Math.floor((e.clientX - rect.left) / rect.width * 100 * 100) / 100 + "%");
                     target.style.setProperty("--y", Math.floor((e.clientY - rect.top) / rect.height * 100 * 100) / 100 + "%");
                     target.classList.toggle("-enter");
