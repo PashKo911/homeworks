50d8b3e1cc0a9b12b77795b52203a173926bc5c2
PashKo911 <rabota.pashok@gmail.com>
2024-11-07 20:51:49 +0200

    git sinc

--- node/7/teaher_tasks/users_manager_LS-7/models/user/UsersDBService.mjs
+++ node/7/teaher_tasks/users_manager_LS-7/models/user/UsersDBService.mjs
@@ -2,39 +2,39 @@ import User from './User.mjs'
 import mongoose from 'mongoose'
 
 class UsersDBService {
-  static async getList() {
-    try {
-      const exists = await User.checkCollectionExists
-      if (exists) {
-        const data = await mongoose.model(collectionName).find().exec()
-        return data
-      }
+	static async getList() {
+		try {
+			const exists = await User.checkCollectionExists()
+			if (exists) {
+				const data = await mongoose.model(collectionName).find().exec()
+				return data
+			}
 
-      return (await User.find({})) ?? []
-    } catch (error) {
-      return []
-    }
-  }
+			return (await User.find({})) ?? []
+		} catch (error) {
+			return []
+		}
+	}
 
-  static async create(data) {
-    const user = new User(data)
-    return await user.save()
-  }
+	static async create(data) {
+		const user = new User(data)
+		return await user.save()
+	}
 
-  static async getById(id) {
-    return await User.findById(id)
-  }
+	static async getById(id) {
+		return await User.findById(id)
+	}
 
-  static async update(id, data) {
-    return await User.findByIdAndUpdate(id, data, {
-      new: true,
-      runValidators: true,
-    })
-  }
+	static async update(id, data) {
+		return await User.findByIdAndUpdate(id, data, {
+			new: true,
+			runValidators: true,
+		})
+	}
 
-  static async deleteById(id) {
-    return await User.findByIdAndDelete(id)
-  }
+	static async deleteById(id) {
+		return await User.findByIdAndDelete(id)
+	}
 }
 
 export default UsersDBService
